c30f9c9a91d73d8f5db42b0657eac131140f2266
liqiang <18257751477@163.com>
2021-02-24 13:51:36 +0800

    n皇后

--- src/algorithm/backtrack/Demo51.java
+++ src/algorithm/backtrack/Demo51.java
@@ -32,11 +32,9 @@ public class Demo51 {
     public List<List<String>> solveNQueens(int n) {
 
         char[][] board = new char[n][n];
-        char[] tmp = new char[n];
-        Arrays.fill(tmp,'.');
-        Arrays.fill(board,tmp);
-
-
+        for (char[] chars : board) {
+            Arrays.fill(chars,'.');
+        }
 
         dfs(board,0);
 
@@ -45,13 +43,87 @@ public class Demo51 {
 
     private void dfs(char[][] board, int row) {
 
+        // 满足了一个条件
+        if(row == board.length) {
+            List<String> list = generator(board);
+            res.add(list);
+            return;
+        }
+
+        int n = board[row].length;
+        for (int col = 0; col < n; col++) {
+            if(!isValid(board,row,col)) {
+                continue;
+            }
+            board[row][col] = 'Q';
+            dfs(board,row+1);
+            board[row][col] = '.';
+        }
+
+
 
     }
 
+    private boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+
+        // 检查列
+        for (int i = 0; i < n; i++) {
+            if(board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // 右上方
+        for (int i = row - 1, j = col + 1; i >=0 && j < n; i--,j++) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // 左上方
+        for (int i = row - 1, j = col - 1; i >=0 && j >= 0; i--,j--) {
+            if(board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<String> generator(char[][] board) {
+        List<String> list = new LinkedList<>();
+        for (char[] chars : board) {
+            StringBuilder sb = new StringBuilder();
+            for (char aChar : chars) {
+                sb.append(aChar);
+            }
+            list.add(sb.toString());
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
-        List<List<String>> lists = new Demo51().solveNQueens(8);
+        List<List<String>> lists = new Demo51().solveNQueens(4);
         System.out.println(lists);
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
